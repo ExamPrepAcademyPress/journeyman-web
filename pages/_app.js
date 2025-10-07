fe41fa37c449852bf7f1924d6d8ea5f4a8a2e24c
ExamPrep Academy Press <examprep.academy.press@gmail.com>
2025-10-07 21:05:12 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 // /pages/_app.js
 import '../styles.css';
+import Head from 'next/head';
 import ReviewGate from '../components/ReviewGate';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ReviewGate>
-      <Component {...pageProps} />
-    </ReviewGate>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <meta name="theme-color" content="#FFD700" />
+      </Head>
+      <ReviewGate>
+        <Component {...pageProps} />
+      </ReviewGate>
+    </>
   );
 }
+
