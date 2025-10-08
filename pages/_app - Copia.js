1c7451b547f5814cffe7f3fc6c538ed3541d55e9
ExamPrep Academy Press <examprep.academy.press@gmail.com>
2025-10-09 01:57:53 +0200

    _app update per favicon

--- pages/_app - Copia.js
+++ pages/_app - Copia.js
@@ -1,18 +1,21 @@
+// /pages/_app.js
 import '../styles.css';
 import Head from 'next/head';
 import ReviewGate from '../components/ReviewGate';
-import CookieConsent from '../components/CookieConsent';
+import CookieConsent from '../components/CookieConsent'; // ✅ aggiunto
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* ✅ favicon compatibile con next export */}
         <link rel="icon" href="/favicon.png" type="image/png" />
         <meta name="theme-color" content="#FFD700" />
       </Head>
 
+      {/* ✅ Banner cookie al pari del ReviewGate */}
       <CookieConsent />
+
+      {/* ✅ Il ReviewGate continua a proteggere tutto */}
       <ReviewGate>
         <Component {...pageProps} />
       </ReviewGate>
